712c154d1b800db9c06ff12ad9e2048c0a6b1ece
= <=>
2015-09-10 14:20:26 +0200

    Ajout dungeon + main

--- src/dungeon/Dungeon.java
+++ src/dungeon/Dungeon.java
@@ -1,12 +1,41 @@
 /**
+ * Dungeon
  * 
  */
 package dungeon;
 
+import java.util.Scanner;
+
 /**
- * @author crosetti
- *
+ * @author Louis
+ * 
  */
 public class Dungeon {
+	private Room currentRoom;
+	private final Scanner scanner = new Scanner(System.in);
 
+	/**
+	 * Create new dungeon with the start room 
+	 * @param firstRoom the entrance room.
+	 */
+	public Dungeon(final Room firstRoom) {
+		this.currentRoom = firstRoom;
+	}
+
+	/**
+	 * Execute the command associate to the input if exists.
+	 * @param command input command from the user
+	 */
+	public void executeCommand(String command) {
+		Room nextRoom = null; // room.getNextRoom(command);
+		if (nextRoom != null) {
+			currentRoom = nextRoom;
+		} else {
+			System.out.println("Oups, il n'y a pas de chemin ici !");
+		}
+	}
+
+	public String readCommand() {
+		return scanner.nextLine();
+	}
 }
